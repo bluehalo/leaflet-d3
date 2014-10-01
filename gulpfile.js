8b12a1cbb6431635058b85a9fd2a4a84f5afbec5
reblace <reblace@gmail.com>
2014-10-01 13:59:43 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+	p = require('./package.json'),
+	gulpLoadPlugins = require('gulp-load-plugins'),
+	plugins = gulpLoadPlugins();
+
+var banner = '/*! ' + p.name + '.js Version: ' + p.version + ' */\n';
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function(){
+	gulp.watch(['src/**/*', '!/src/lib/**/*'], ['build']);
+});
+
+gulp.task('build', ['js'] );
+
+gulp.task('js', function(){
+	return gulp.src('src/js/**/*.js')
+
+		// JS Hint
+		.pipe(plugins.jshint('.jshintrc'))
+		.pipe(plugins.jshint.reporter('jshint-stylish'))
+
+		// Concatenate
+		.pipe(plugins.concat('leaflet-hexbin.js'))
+		.pipe(plugins.insert.prepend(banner))
+		.pipe(gulp.dest('dist'))
+		.pipe(plugins.filesize())
+
+		// Uglify
+		.pipe(plugins.uglify())
+		.pipe(plugins.rename('leaflet-hexbin.min.js'))
+		.pipe(plugins.insert.prepend(banner))
+		.pipe(gulp.dest('dist'))
+		.pipe(plugins.filesize())
+		.on('error', plugins.util.log);
+});
