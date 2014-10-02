2a73a3a1aa3680d1978ac7b147d3a5b20c79a1a9
reblace <reblace@gmail.com>
2014-10-01 20:31:48 -0400

    Making some mods to the options.

--- README.md
+++ README.md
@@ -5,6 +5,15 @@
 ## What is it?
 This is a Leaflet plugin that enables you to place a d3.js hexbin heatmap overlay onto your maps. This plugin is based on [the work of Steven Hall](http://www.delimited.io/blog/2013/12/1/hexbins-with-d3-and-leaflet-maps). The primary difference is that this plugin leverages the data-binding power of d3 to allow you to dynamically update the data and visualize the transitions.
 
+## How do I use it?
+To use, simply declare a hexbin layer and add it to your map. You can then add data to the layer.
+
+```js
+var hexLayer = L.hexbinLayer({}).addTo(map);
+hexLayer.update([...data...]);
+```
+There are several customizations. You can override the way that data is accessed by providing accessor functions.
+
 ## How do I include this plugin in my project?
 The easiest way to include this plugin in your project, use [Bower](http://bower.io)
 
@@ -31,15 +40,6 @@ npm install
 bower install
 gulp
 ```
-
-## Documentation
-To use, simply declare a hexbin layer and add it to your map. You can then add data to the layer.
-
-```js
-var hexLayer = L.hexbinLayer({}).addTo(map);
-hexLayer.update([...data...]);
-```
-There are several customizations. You can override the way that data is accessed by providing accessor functions.
 
 ## Credits
 This plugin was based on [the work of Steven Hall](http://www.delimited.io/blog/2013/12/1/hexbins-with-d3-and-leaflet-maps).
--- dist/leaflet-hexbin.js
+++ dist/leaflet-hexbin.js
@@ -3,67 +3,59 @@
 	"use strict";
 
 	L.HexbinLayer = L.Class.extend({
-		includes : L.Mixin.Events,
-	
+
 		options : {
 			radius : 10,
-			padding: 100,
-			minZoom: 1,
-			maxZoom: 18,
-			coord: function(d){
+			opacity: 0.5,
+			geo: function(d){
 				return d.geo;
 			},
-			x: function(d){
+			lng: function(d){
 				return d[0];
 			},
-			y: function(d){
+			lat: function(d){
 				return d[1];
 			},
 			colorRange: ['#f7fbff', '#08306b']
 		},
-	
+
 		initialize : function(options) {
 			L.setOptions(this, options);
-			this._layout = d3.hexbin().radius(this.options.radius);
+
+			this._hexLayout = d3.hexbin().radius(this.options.radius);
 			this._data = [];
-			this._levels = {};
 			this._colorScale = d3.scale.linear().range(this.options.colorRange);
 		},
 	
 		onAdd : function(map) {
 			this._map = map;
-	
+
 			// Create a container for svg.
 			this._container = this._initContainer();
-	
+
 			// Set up events
 			map.on({'moveend': this._redraw}, this);
-	
+
 			// Initial draw
 			this._redraw();
 		},
-	
+
 		onRemove : function(map) {
 			this._destroyContainer();
-	
+
 			// Remove events
 			map.off({'moveend': this._redraw}, this);
-	
+
 			this._container = null;
 			this._map = null;
 			this._data = null;
 		},
-	
+
 		addTo : function(map) {
 			map.addLayer(this);
 			return this;
 		},
-	
-		update : function(data){
-			this._data = (null != data)? data : [];
-			this._redraw();
-		},
-	
+
 		_initContainer : function() {
 			var container = null;
 	
@@ -93,7 +85,7 @@
 			var zoom = this._map.getZoom();
 	
 			// Determine the bounds from the data and scale the overlay
-			var padding = this.options.padding;
+			var padding = this.options.radius * 2;
 			var geoBounds = this._getBounds(this._data);
 			var bounds = this._translateBounds(geoBounds);
 			var width = bounds.getSize().x + (2 * padding),
@@ -101,17 +93,20 @@
 				marginTop = bounds.min.y - padding,
 				marginLeft = bounds.min.x - padding;
 	
-			this._layout.size([ width, height ]);
+			this._hexLayout.size([ width, height ]);
 			this._container
 				.attr('width', width).attr('height', height)
 				.style('margin-left', marginLeft + 'px')
 				.style('margin-top', marginTop + 'px');
 	
-			// Select the hex group for the current zoom level
-			var join = this._container.selectAll('g.hexbin').data([zoom], function(d){ return d; });
+			// Select the hex group for the current zoom level. This has 
+			// the effect of recreating the group if the zoom level has changed
+			var join = this._container.selectAll('g.hexbin')
+				.data([zoom], function(d){ return d; });
 	
 			// enter
-			join.enter().append('g').attr('class', function(d) { return 'hexbin zoom-' + d; });
+			join.enter().append('g')
+				.attr('class', function(d) { return 'hexbin zoom-' + d; });
 	
 			// enter + update
 			join.attr('transform', 'translate(' + -marginLeft + ',' + -marginTop + ')');
@@ -129,76 +124,87 @@
 	
 			// Generate the mapped version of the data
 			var data = that._data.map(function(d) {
-				var coord = that.options.coord(d);
-				var x = that.options.x(coord);
-				var y = that.options.y(coord);
+				var geo = that.options.geo(d);
+				var lng = that.options.lng(geo);
+				var lat = that.options.lat(geo);
 	
-				return that._project([x, y]);
+				return that._project([lng, lat]);
 			});
 	
 			// Create the bins using the hexbin layout
-			var bins = that._layout(data);
+			var bins = that._hexLayout(data);
 			that._colorScale.domain([0, bins.reduce(function(val, element){
 				return Math.max(val, element.length);
 			}, 0)]);
 	
 			// Update the d3 visualization
-			var join = g.selectAll('path.hexbin-hexagon').data(bins, function(d){ return d.i + ':' + d.j; });
-			join.transition().duration(200).attr('fill', function(d){ return that._colorScale(d.length); });
+			var join = g.selectAll('path.hexbin-hexagon')
+				.data(bins, function(d){ return d.i + ':' + d.j; });
+
+			join.transition().duration(200)
+				.attr('fill', function(d){ return that._colorScale(d.length); });
 	
 			join.enter().append('path').attr('class', 'hexbin-hexagon')
 				.attr('d', function(d){
-					return 'M' + d.x + ',' + d.y + that._layout.hexagon();
+					return 'M' + d.x + ',' + d.y + that._hexLayout.hexagon();
 				})
 				.attr('fill', function(d){ return that._colorScale(d.length); })
 				.attr('opacity', 0.01)
 				.transition().duration(200)
-				.attr('opacity', 0.4);
+				.attr('opacity', that.options.opacity);
 	
 			join.exit().transition().duration(200)
 				.attr('opacity', 0.01)
 				.remove();
 		},
-	
+
 		_project : function(coord) {
 			var point = this._map.latLngToLayerPoint([ coord[1], coord[0] ]);
 			return [ point.x, point.y ];
 		},
-	
+
 		_translateBounds : function(bounds) {
 			var nw = this._project([ bounds[0][0], bounds[1][1] ]), 
 				se = this._project([ bounds[1][0], bounds[0][1] ]);
 			return L.bounds(nw, se);
 		},
-	
+
 		_getBounds: function(data){
 			var that = this;
-	
+
 			if(null == data || data.length < 1){
 				return [[0, 0], [0, 0]];
 			}
-	
+
 			// bounds is [[min long, min lat], [max long, max lat]]
 			var bounds = [[999, 999], [-999, -999]];
-	
+
 			data.forEach(function(element){
-				var coord = that.options.coord(element);
-				var x = that.options.x(coord);
-				var y = that.options.y(coord);
-	
-				bounds[0][0] = Math.min(bounds[0][0], x);
-				bounds[0][1] = Math.min(bounds[0][1], y);
-				bounds[1][0] = Math.max(bounds[1][0], x);
-				bounds[1][1] = Math.max(bounds[1][1], y);
+				var geo = that.options.geo(element);
+				var lng = that.options.lng(geo);
+				var lat = that.options.lat(geo);
+
+				bounds[0][0] = Math.min(bounds[0][0], lng);
+				bounds[0][1] = Math.min(bounds[0][1], lat);
+				bounds[1][0] = Math.max(bounds[1][0], lng);
+				bounds[1][1] = Math.max(bounds[1][1], lat);
 			});
-	
+
 			return bounds;
+		},
+
+		/* 
+		 * This is the method that changes the data array
+		 */
+		update : function(data){
+			this._data = (null != data)? data : [];
+			this._redraw();
 		}
-	
+
 	});
 
-	L.hexbinLayer = function(data, options) {
-		return new L.HexbinLayer(data, options);
+	L.hexbinLayer = function(options) {
+		return new L.HexbinLayer(options);
 	};
 
 })();
