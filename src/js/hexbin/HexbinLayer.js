9bb740f822e6f3651eba84c801c1e19992315209
reblace <reblace@gmail.com>
2014-12-03 16:17:39 -0500

    Fixing issue #3

--- src/js/hexbin/HexbinLayer.js
+++ src/js/hexbin/HexbinLayer.js
@@ -164,13 +164,13 @@
 
 			// Update - set the fill and opacity on a transition (opacity is re-applied in case the enter transition was cancelled)
 			join.transition().duration(that.options.duration)
-				.attr('fill', function(d){ return that._colorScale(d.length); })
+				.attr('fill', function(d){ return that._colorScale(that.options.value(d)); })
 				.attr('opacity', that.options.opacity);
 	
 			// Enter - establish the path, the fill, and the initial opacity
 			join.enter().append('path').attr('class', 'hexbin-hexagon')
 				.attr('d', function(d){ return 'M' + d.x + ',' + d.y + that._hexLayout.hexagon(); })
-				.attr('fill', function(d){ return that._colorScale(d.length); })
+				.attr('fill', function(d){ return that._colorScale(that.options.value(d)); })
 				.attr('opacity', 0.01)
 				.transition().duration(that.options.duration)
 					.attr('opacity', that.options.opacity);
